494a7c45f67f84f6efb76895f8e5c47385d13e57
Ferhat Güdek <ferhat.gudek10@gmail.com>
2025-10-22 00:14:47 +0300

    Decision Tree Classifier work with python

---
+++ DTC.py
@@ -0,0 +1,67 @@
+import matplotlib.pyplot as plt
+import pandas as pd 
+import numpy as np
+
+veriler = pd.read_csv("eksikveriler.csv")
+print(veriler)
+
+x = veriler.iloc[:,1:4].values # bağımsız değişkenler
+y = veriler.iloc[:,4:].values  # bağımlı değişkenler
+
+print(x)
+print(y)
+
+from sklearn.model_selection import train_test_split
+
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.33,random_state=0)
+
+ # şimdi sıra verileri ölçeklendirmekte
+ 
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(x_train)
+X_test = sc.transform(x_test)
+
+# şimdi daha çok sayısal verilerden kategorik verilere dönüşümde kullanılan lojistik regression kullanılacak
+
+from sklearn.linear_model import LogisticRegression
+
+logr = LogisticRegression(random_state=0)
+logr.fit(X_train,y_train)
+
+y_pred = logr.predict(X_test)
+print(y_pred)
+print(y_test)
+
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test,y_pred)
+print(cm)
+
+# tahminin başarılı olduğunu söyleyemeyiz ancak yine de çalışmış oldu
+
+from sklearn.neighbors import KNeighborsClassifier
+
+knn = KNeighborsClassifier(n_neighbors=1,metric="minkowski")
+knn.fit(X_train,y_train)
+y_pred = knn.predict(X_test)
+cm = confusion_matrix(y_test,y_pred)
+print(cm)
+# bu durumda olduğu gibi bazen komşu sayısı artırmak yerine azaltmak daha iyi sonuçlara yol açabilir
+
+from sklearn.naive_bayes import GaussianNB
+gnb = GaussianNB()
+gnb.fit(X_train,y_train)
+y_pred = gnb.predict(X_test)
+cm = confusion_matrix(y_test,y_pred)
+print("GNB")
+print(cm) # 8 tahmninden birini doğru tahmin ettiği gözlenen bir model oluşturduk tabi biz burada gauisian seçtik
+# başka seçimlerde olabilirdi o zaman doğruluk durumu da değişir en uygunu seçebilirdik
+
+from sklearn.tree import DecisionTreeClassifier
+dtc = DecisionTreeClassifier(criterion="entropy")
+dtc.fit(X_train,y_train)
+y_pred= dtc.predict(X_test)
+cm = confusion_matrix(y_test,y_pred)
+print("DTC")
+print(cm)
+# cm sonucuna göre oldukça başarılı bir sonuç yakaladığımız söylenebilir
